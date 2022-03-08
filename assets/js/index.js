475b0f8483c28e962a86cb79ef5feb3c04378779
aba665 <abissons@gmail.com>
2022-03-08 15:54:10 -0300

    Jogo pronto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,10 @@
+let barraPlaneta = document.getElementById("barraPlaneta");
+let statusPlaneta = document.getElementById("statusPlaneta");
+let placarPlaneta = document.getElementById("placar");
+let allBomb = document.getElementById("bombasRestante");
+let playerAgain = document.querySelector(".teste");
+let msgFinal = document.querySelector("#msgFinal");
+let telaMsg = document.querySelector("#telaMsg");
 let directionxJ;
 let directionyJ;
 let jogo;
@@ -9,11 +16,15 @@ let telaHeight;
 let player;
 let velPlayer;
 let velFire;
+let velAsteroid;
+let timeBomba;
+let qntBombas;
+let lifePlaneta;
+let pontuation;
 
 
 function teclaDown(){
     let tecla = event.keyCode;
-    // console.log('tecla down clicada');
     if(tecla == 38){
         directionyJ += -1;
     } else if(tecla == 40){
@@ -55,13 +66,13 @@ function onFire(x, y){
 function controllerFire(){
     let fires = document.getElementsByClassName("drawnFire");
     let tamFire = fires.length;
-
+    
     for(let i = 0; i < tamFire; i++) {
         if(fires[i]){
             let ptFire = fires[i].offsetTop;
             ptFire -= velFire;
             fires[i].style.top = `${ptFire}px`;
-    
+            destroyAsteroid(fires[i]);
         if(ptFire < 0){
             fires[i].remove();
         }
@@ -77,10 +88,79 @@ function contollerPlayer(){
     player.style.left = positionxJ + "px";
 }
 
+function creationAsteroid(){
+    if(jogo){
+        let numRandom = Math.random() * telaWidth;
+        let asteroid = document.createElement("div");
+        asteroid.setAttribute('class', 'asteroidForm');
+        asteroid.setAttribute("style", `left: ${numRandom}px`)
+        document.body.appendChild(asteroid);
+        qntBombas--;
+        allBomb.textContent = `Bombas Restantes = ${qntBombas}`
+
+        if(qntBombas <= 0 && barraPlaneta.style.width >= 0){
+            msgFinal.textContent = "Meus parabens você conseguiu salvar o planeta!"
+            jogo = false;
+            playerAgain.style.display = "flex";
+        }
+    }
+}
+
+function controllerAsteroid(){
+   
+    let allAsteroid = document.getElementsByClassName("asteroidForm");
+    let tamAsteroid = allAsteroid.length;
+
+    for(let i = 0; i < tamAsteroid; i++) {
+        if(allAsteroid[i]){
+            let ptAsteroid = allAsteroid[i].offsetTop;
+            ptAsteroid += velAsteroid;
+            allAsteroid[i].style.top = `${ptAsteroid}px`;
+    
+        if(ptAsteroid > telaHeight){
+            allAsteroid[i].remove();
+            lifePlaneta -= 16.6;
+            statusPlaneta.textContent = `Vida do Planeta: ${lifePlaneta.toFixed(2)}/250`;
+            barraPlaneta.style.width = `${lifePlaneta}px`
+            if(lifePlaneta <= 0){
+                statusPlaneta.textContent = 'Vida do Planeta: 0/250';
+                jogo = false;
+                msgFinal.textContent = "Não foi dessa vez, tente mais uma vez!"
+                playerAgain.style.display = "flex";
+            }
+        }
+    }
+    }
+}
+
+function destroyAsteroid(fire){
+    let allAsteroid = document.getElementsByClassName("asteroidForm");
+    let tamAsteroid = allAsteroid.length;
+    for(let i = 0; i < tamAsteroid; i++){
+        if(allAsteroid[i]){
+            if(
+                //lado de baixo do asteroide com a ponta do tiro e lado de baixo do tiro com lado de cima da bomba
+                ((fire.offsetTop) <= (allAsteroid[i].offsetTop + 60)) && ((fire.offsetTop + 8) >= (allAsteroid[i].offsetTop))
+                &&
+                //esquerda tiro com direita da bomba e direita tiro com esquerda da bomba
+                (fire.offsetLeft <= (allAsteroid[i].offsetLeft + 60)) && ((fire.offsetLeft + 8) >= (allAsteroid[i].offsetLeft))
+            ){
+            
+                pontuation += 1
+                placarPlaneta.textContent = `Placar = ${pontuation}`;
+                allAsteroid[i].remove();
+                fire.remove();
+            }
+        
+            }
+        }
+}
 function looping(){
     if(jogo){
         contollerPlayer();
         controllerFire();
+        controllerAsteroid();
+        
     } 
     frames = requestAnimationFrame(looping);
 
@@ -95,15 +175,34 @@ function inicia(){
     directionyJ = 0;
     positionxJ = telaWidth / 2;
     positionyJ = telaHeight / 2;
-    velPlayer = 0.3;
+    velPlayer = 0.5;
     player = document.getElementById("navJog");
     player.style.top = positionyJ + "px";
     player.style.left = positionxJ + "px";
     velFire = 5;
-    
-    looping();
-}
+    velAsteroid = 1;
+    lifePlaneta = 250;
+    qntBombas = 150;
+    pontuation = 0;
 
+    statusPlaneta.textContent = 'Vida do Planeta: 250';
+    placarPlaneta.textContent = `Placar = ${pontuation}`;
+    allBomb.textContent = `Bombas Restantes = ${qntBombas}`;
+
+    clearInterval(timeBomba);
+    looping();
+   
+      
+       
+    
+}
+function atualization(){
+    location.reload();
+}
+function startGame(){
+     telaMsg.style.display = "none"
+     timeBomba = setInterval(creationAsteroid, 800);
+}
 window.addEventListener("load", inicia);
 document.addEventListener("keydown", teclaDown);
 document.addEventListener("keyup", teclaUp);
